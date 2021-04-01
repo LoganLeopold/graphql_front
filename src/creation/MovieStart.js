56121becd30d585e8fd70e11c5ff6e8122f876f8
Logan Leopold <loganleopld@me.com>
2021-04-01 12:31:13 -0400

    need to transition moviestart with axios now

--- src/creation/MovieStart.js
+++ src/creation/MovieStart.js
@@ -5,12 +5,19 @@ class MovieStart extends Component {
         return (
             <div>
                 <form action="http://localhost:8000/movie/create" method="post">
+                    <label>Title</label>
                     <input name="name" value={this.value} id="movie_name"/>
+                    <label>Director</label>
                     <input name="director" value={this.value} id="director_name"/>
+                    <label>Actors</label>
                     <input name="actors" value={this.value} id="actor_names"/>
+                    <label>Platforms</label>
                     <input name="platforms" value={this.value} id="platform_names"/>
+                    <label>Rotten Tomatoes Audience Score</label>
                     <input name="tom_pub" value={this.value} id="tom_pub"/>
-                    <input name="tom_priv" value={this.value} id="tom_priv"/>
+                    <label>Rotten Tomatoes Critic Score</label>
+                    <input name="tom_crit" value={this.value} id="tom_priv"/>
+                    <label>Genres</label>
                     <input name="genres" value={this.value} id="genres"/>
                     <input type="submit" value="submit" /> 
                 </form>
