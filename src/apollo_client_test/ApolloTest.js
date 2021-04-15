12dbb8286b9488d28cc86962a0ca429835b5aa4e
Logan Leopold <loganleopld@me.com>
2021-04-15 14:41:02 -0400

    a quick mutation test

--- src/apollo_client_test/ApolloTest.js
+++ src/apollo_client_test/ApolloTest.js
@@ -2,11 +2,20 @@ import { useQuery, gql } from '@apollo/client';
 
 const ApolloTest = (props) => {
 
+    // const allActors = gql`
+    //     query actorMany {
+    //         actorMany {
+    //         Name,
+    //         _id
+    //         }
+    //     }
+    // `
+    const test = "Matthew Damon"
+
     const allActors = gql`
-        query actorMany {
-            actorMany {
-            Name,
-            _id
+        mutation {
+            actorUpdateOne(filter:{_id:"606b4a0f86b9b68d59f576e5"}, record:{Name:"${test}"}) {
+            recordId
             }
         }
     `
@@ -19,7 +28,6 @@ const ApolloTest = (props) => {
         return <p>Error :(</p>;
     }
 
-    console.log(data)
     return data.actorMany.map( actor => {
         return <p key={actor.Name}>{actor.Name}</p>
     })
