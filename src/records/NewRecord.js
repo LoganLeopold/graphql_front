e086752e94e353ca8b52b660441d81215a75095c
Logan Leopold <loganleopld@me.com>
2021-05-17 19:48:44 -0400

    code not working but some good newrecord.js pseudocode

--- src/records/NewRecord.js
+++ src/records/NewRecord.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react'
-// import { useMutation, gql } from '@apollo/client';
+import { useMutation, gql } from '@apollo/client';
 // import { capitalize } from '../utilities'
 
 const NewRecord = (props) => {
 
     const [editing, openEditing] = useState(false)
+
+    const { propObj: { recordData, currentData }} = props 
+
+    let update;
+
+    let update = recordData.modelName ? 
+    // If model, do ref update
+    gql`
+        mutation {
+            // with ref doc
+        }
+    `
+    // If no model, do simple update
+    gql`
+        mutation {
+            // simple update 
+        }
+    `
+
+    let 
+
+    const [updateDoc, { loading, error }] = useMutation(update, {
+        onCompleted(data) {
+            if (data) {    
+                console.log(data)
+            } else if (deleteLoading) {
+                console.log("loading")
+            } else if (deleteError) {
+                console.log(deleteError)
+            }   
+        }
+    })
 
     const revealInput = (e) => {
         openEditing(!editing)
