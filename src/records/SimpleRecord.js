2f1c6e977488f49c7450f9f87902e8cddb9c414f
Logan Leopold <loganleopld@me.com>
2021-06-02 11:41:27 -0400

    simplerecord update is pretty hype

--- src/records/SimpleRecord.js
+++ src/records/SimpleRecord.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMutation, gql } from '@apollo/client';
 import { capitalize } from '../utilities';
 
 const SimpleRecord = (props) => {
 
-    const [editing, openEditing] = useState(false)
-    const [inputValue, changeValue] = useState()
-    
     const { propObj: { subDoc, currentDocData } } = props
     let field = currentDocData ? Object.keys(subDoc)[0] : "No Records"
     let initialValue = currentDocData ? Object.values(subDoc)[0] : "No Records"
     let currentDocModel = currentDocData.modelName ? capitalize(currentDocData.modelName, 0, 1) : "placehold"
-
+    const [editing, openEditing] = useState(false)
+    const [inputValue, changeValue] = useState(initialValue)
+    
     let recUpdate = gql`
-        mutation simple${currentDocModel}UpdateHandle ($movieId:MongoID!, $field: String!, $value: String!) {
-            simpleMoviesUpdateHandle (movieId: $movieId, field: $field, value: $value) {
-                _id
-                name
-                genres
-            }   
-        } 
+    mutation simple${currentDocModel}UpdateHandle ($movieId:MongoID!, $field: String!, $value: String!) {
+        simple${currentDocModel}UpdateHandle (movieId: $movieId, field: $field, value: $value) {
+            _id
+            name
+            genres
+        }   
+    } 
     `
-
+    
     const handleChange = (e) => {
         changeValue(e.target.value)
     }
+    // useEffect( () => {}, [inputValue])
     
     const [updateRecord, { updateLoading, updateError }] = useMutation( recUpdate, {
         onCompleted(data) {
             if (data) {
-                console.log(data)
-                props.refreshParent()
+                openEditing(false)
+                props.refreshParent(currentDocData._id)
             } else if (updateLoading) {
                 console.log("loading")
             } else if (updateError) {
@@ -41,8 +41,6 @@ const SimpleRecord = (props) => {
 
     const updateRecordEvent = (event) => {
         event.preventDefault()
-        console.log("it was a number. sick.")
-        console.log(currentDocData._id)
         updateRecord({ variables: {
             movieId: currentDocData._id,
             field: field,
@@ -58,7 +56,7 @@ const SimpleRecord = (props) => {
     return (
         <form className="record" onSubmit={updateRecordEvent}>
             <h3 >{initialValue}</h3>
-            { editing && currentDocData && (<input onChange={handleChange}/> )}
+            { editing && currentDocData && (<input onChange={handleChange} defaultValue={initialValue} /> )}
             { editing && currentDocData && (<input type="submit" value="+" />)}
             { currentDocData && <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/Ei-pencil.svg" alt="Pencil for edit" onClick={revealInput}></img>}
         </form>
